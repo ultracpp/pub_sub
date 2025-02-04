28df45862f2dcc1b1fb7730825edf94112042d09
The Solitary Dev <ultracpp@naver.com>
2025-02-04 12:47:49 +0900

    Update pub_sub_server.rs

--- pub_sub_server/src/pub_sub_server.rs
+++ pub_sub_server/src/pub_sub_server.rs
@@ -1,3 +1,19 @@
+/*
+ * rust-pub-sub-server - A high-performance Pub/Sub server built with Rust
+ * Copyright (c) 2024 Your Name
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 use bytes::{Bytes, BytesMut};
 use config::Config;
 use once_cell::sync::Lazy;
